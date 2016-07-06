68ce2e07fc6601d922a01ca557e0dbdc552b2bad
designpancho <designpancho@gmail.com>
2016-07-06 17:10:38 -0400

    95%

---
+++ application/views/administrador/agregarDisco.php
@@ -0,0 +1,47 @@
+<table>
+    <tr>
+        <td>id_Disco*</td>
+        <td>: <input id="addid_Disco"  onblur="validaDiscoID(this.value)" placeholder="id Disco"/></td>
+    </tr>
+    <tr>
+        <td>Nombre*</td>
+        <td>: <input id="addnombre" placeholder="Nombre" /></td>
+    </tr>
+<!--     <tr>          
+         <td colspan="2" align="center"><div id="addNombreMensaje"></div></td>
+     </tr>-->
+    <tr>
+        <td>Descripcion*</td>
+        <td>: <input id="addDescripcion" placeholder="Descripcion"/></td>
+    </tr>
+<!--    <tr>          
+         <td colspan="2" align="center"><div id="addDescripcionMensaje"></div></td>
+     </tr>-->
+    <tr>
+        <td>Interprete*</td>
+        <td>: <input id="addIntegrante" placeholder="Interprete"/></td>
+    </tr>
+<!--    <tr>          
+         <td colspan="2" align="center"><div id="addInterpreteMensaje"></div></td>
+     </tr>-->
+    <tr>
+        <td>Fecha*</td>        
+        <td>: <input type="date" id="addFecha"></td>
+        
+    </tr>
+<!--    <tr>          
+         <td colspan="2" align="center"><div id="addFechaMensaje"></div></td>
+     </tr>-->
+     <tr>
+        <td>Valor*</td>
+        <td>: <input id="addValor" type="number"  placeholder="Valor"/></td>
+    </tr>
+    <tr>          
+         <td colspan="2" align="center"><div id="addDiscoMensaje"></div></td>
+     </tr>
+<!--     <tr>
+        <td>imagenes</td>
+        <td>: <input id="file" type="file" placeholder=""/></td>
+    </tr>-->
+</table>
+ <div id="mensajesAdd"></div>
